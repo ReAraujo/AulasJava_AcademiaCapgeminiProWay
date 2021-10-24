97008282f53d724670fb8dbb486ee79705e181bc
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-24 16:55:41 -0300

    aula 26 - Tratamento de Exceções

---
+++ src/Modulo02/Aula26_TratamentoDeExcecao/Main.java
@@ -0,0 +1,21 @@
+package Modulo02.Aula26_TratamentoDeExcecao;
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = null;
+        //nome.length();
+        // Exception in thread "main" java.lang.NullPointerException: Cannot invoke "String.length()" because "nome" is null
+
+        int idade = Integer.parseInt("-10abc"); // NumberFormatException
+
+        if(idade <= 0 || idade > 150){
+            throw new IllegalArgumentException("Idade inválida!"); // Lançando uma exceção
+        }
+        // Exception in thread "main" java.lang.IllegalArgumentException: Idade inválida!
+        
+
+
+
+    }
+    
+}
