3a639b11bb5e7bcdc7bf093a67b090162ec72976
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-16 10:41:45 -0300

    ajuste - aulas

---
+++ src/Modulo03/Aula37/view/View2.java
@@ -0,0 +1,30 @@
+package Modulo03.Aula37.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import Modulo03.Aula35_Pool_ConnectionFactory.utils.ConnectionFactory;
+
+public class View2 {
+    public static void main(String[] args) {
+        try(Connection conn = new ConnectionFactory().getConnection()) {
+            String nome = "UpdatePrepStatment";
+            int id = 25;
+            
+            String sql = "UPDATE Categoria SET nome = ? where id = ? ";
+            PreparedStatement prepStatement = conn.prepareStatement(sql);
+            prepStatement.setString(1, nome);
+            prepStatement.setInt(2, id);
+
+            prepStatement.execute();
+            
+            int linhasAfetadas = prepStatement.getUpdateCount();
+            System.out.println(linhasAfetadas);      
+        } catch (SQLException e) {
+            System.out.println("Não foi possível conectar.");
+        }
+
+    }
+    
+}
---
+++ src/Modulo03/Aula37/view/View3.java
@@ -0,0 +1,32 @@
+package Modulo03.Aula37.view;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Modulo03.Aula35_Pool_ConnectionFactory.utils.ConnectionFactory;
+
+public class View3 {
+    public static void main(String[] args) {
+        try(Connection conn = new ConnectionFactory().getConnection()) {
+            String nome = "TestePrepared";
+            
+            String sql = "INSERT INTO categoria(nome)values(?)";
+            PreparedStatement prepStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            prepStatement.setString(1, nome);
+
+            prepStatement.execute();            
+            ResultSet ids = prepStatement.getGeneratedKeys();
+
+            while(ids.next()){
+                int id = ids.getInt("id");
+                System.out.println(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
